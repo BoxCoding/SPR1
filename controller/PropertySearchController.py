def0d6092e3080de0de4eff83d54e9085a99adfd
Vishal Sharma <boxcodingtech@gmail.com>
2020-02-19 15:46:23 +0530

    Initial commit

---
+++ controller/PropertySearchController.py
@@ -0,0 +1,19 @@
+from flask import Flask,jsonify,request
+
+app=Flask(__name__)
+
+
+@app.route('/sprsearch', methods=['POST'])
+def getSearch():
+    dataDict=request.get_json()
+    print(dataDict)
+    retJson={
+        "final":dataDict
+    }
+    return jsonify(retJson),200
+
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
