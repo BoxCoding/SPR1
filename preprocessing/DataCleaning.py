b1659d99564ba82dcf68324aa7d1525fa3d92d42
Vishal Sharma <boxcodingtech@gmail.com>
2020-03-05 23:59:34 +0530

    update datacleaning file

--- preprocessing/DataCleaning.py
+++ preprocessing/DataCleaning.py
@@ -1,10 +1,10 @@
-import pandas as pd
-import numpy as np
-from nltk.stem.wordnet import WordNetLemmatizer
-from collections import Counter
+import nltk
+nltk.download('vader_lexicon')
+nltk.download('stopwords')
 from nltk.corpus import stopwords
+from nltk.stem import SnowballStemmer
+from nltk.tokenize import RegexpTokenizer
 import re
-import math
 
 
 REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
@@ -13,41 +13,65 @@ url_symbol=re.compile('[^https?:\/\/.*[\r\n]*]')
 STOPWORDS = set(stopwords.words('english'))
 
 
-def clean_text(text):
-    """
-        text: a string
-
-        return: modified initial string
-    """
-    text = text.lower()  # lowercase text
-    text = REPLACE_BY_SPACE_RE.sub(' ',           text)  # replace REPLACE_BY_SPACE_RE symbols by space in text. substitute the matched string in REPLACE_BY_SPACE_RE with space.
-    text = BAD_SYMBOLS_RE.sub('',
-                              text)  # remove symbols which are in BAD_SYMBOLS_RE from text. substitute the matched string in BAD_SYMBOLS_RE with nothing.
-    text = url_symbol.sub('', text)
-    text = ' '.join(word for word in text.split() if word not in STOPWORDS)  # remove stopwors from text
-    return text
-
-def pre_process(text):
-    # lowercase
-    text = text.lower()
-
-    # remove tags
-    text = re.sub("</?.*?>", " <> ", text)
-
-    # remove special characters and digits
-    text = re.sub("(\\d|\\W)+", " ", text)
-
+@staticmethod
+def stem_words(text):
+    text = text.split()
+    stemmer = SnowballStemmer('english')
+    stemmed_words = [stemmer.stem(word) for word in text]
+    text = " ".join(stemmed_words)
     return text
 
 
-def get_stop_words(stop_file_path):
-    """load stop words """
-
-    with open(stop_file_path, 'r', encoding="utf-8") as f:
-        stopwords = f.readlines()
-        stop_set = set(m.strip() for m in stopwords)
-        return frozenset(stop_set)
+@staticmethod
+def make_lower_case(text):
+    return text.lower()
 
 
+@staticmethod
+def remove_stop_words(text):
+    text = text.split()
+    stops = set(stopwords.words("english"))
+    text = [w for w in text if not w in stops]
+    text = " ".join(text)
+    return text
 
+
+@staticmethod
+def normalize(s):
+    """
+    Given a text, cleans and normalizes it. Feel free to add your own stuff.
+    """
+    s = s.replace('&', ' and ')
+    s = s.replace('@', ' at ')
+
+    return s
+
+
+@staticmethod
+def remove_punctuation(text):
+    tokenizer = RegexpTokenizer(r'\w+')
+    text = tokenizer.tokenize(text)
+    text = " ".join(text)
+    return text
+
+@staticmethod
+def replace_ftk(x):
+    #print(x)
+    if(re.findall(r'FTK',x)):
+        result = re.sub(r'FTK','',x)
+    else:
+        result = x
+    return (result)
+
+@staticmethod
+def replace_int(x):
+    #print(x)
+    if x !='nan':
+        if(re.findall("[$&+,:;=?@#|'<>.^*()%!-]",x)):
+            result = re.sub("[$&+,:;=?@#|'<>.^*()%!-]",'',x)
+        else:
+            result = x
+    else:
+        result =0
+    return result
 
--- preprocessing/__init__.py
+++ preprocessing/__init__.py
@@ -1,11 +1,34 @@
 import math
 
 
+class data_processing:
+
+    @staticmethod
+    def filter_intvalue(df,col):
+        df[col] = df[col].fillna(0)
+        return df
+
+    @staticmethod
+    def filter_strvalue(df,col):
+        df[col] = df[col].fillna('')
+        return df
+
+    @staticmethod
+    def filter_floorno(x):
+        if x == "Ground":
+            x = 0
+        elif x == "Upper Basement":
+            x = -1
+        elif x == "Lower Baserment":
+            x = -2
+        return x
 
 
-def counter_cosine_similarity(c1, c2):
-    terms = set(c1).union(c2)
-    dotprod = sum(c1.get(k, 0) * c2.get(k, 0) for k in terms)
-    magA = math.sqrt(sum(c1.get(k, 0)**2 for k in terms))
-    magB = math.sqrt(sum(c2.get(k, 0)**2 for k in terms))
-    return dotprod / (magA * magB)
+
+    @staticmethod
+    def counter_cosine_similarity(c1, c2):
+        terms = set(c1).union(c2)
+        dotprod = sum(c1.get(k, 0) * c2.get(k, 0) for k in terms)
+        magA = math.sqrt(sum(c1.get(k, 0)**2 for k in terms))
+        magB = math.sqrt(sum(c2.get(k, 0)**2 for k in terms))
+        return dotprod / (magA * magB)
